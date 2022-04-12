ca5a006b57117795d39b6b723127648b21e137e3
Jamond Turner <jamondturner@yahoo.com>
2022-04-12 12:37:49 -0400

    Better Not Yet Complete

--- deli-counter.js
+++ deli-counter.js
@@ -1,99 +1,44 @@
-//////////STEP #1 Create an array assign the variable katzDeli//////////////
+// / Represent queue with an empty array
+
 const katzDeli = [];
-const customerNames = ['Ada', 'Grace', 'Kent']
 
-function takeANumber(katzDeli, name) {
-    return message
-}
-console.log(`Welcome, ${name} You are number 1 in line.}`)
+//  Build a line function that takes in and array (Think INPUT/Parameter an array)
+//  containing everyone with their current place in line.  If nobody is in line (array.length = 0)
+//  it should say (Think console.log) "The line is currently empty.
+// example line(katzDeli) "The line is currently: 1. Ada 2. Grace 3. Kent"
 
-function takeANumber(katzDeli, 'Ada');
-
-console.log("Welcome, Ava. You are number 1 in line.")
-
-function takeANumber(katzDeli, 'Grace');
-
-console.log("Welcome, Grace. You are number 2 in line.")
-
-function takeANumber(katzDeli, 'Kent');
-
-console.log("Welcome, Kent. You are number 3 in line.")
-
-
-// /////////////////////////////////////////////////////////////////////////////////////////////
-
-
-function takeANumber(katzDeli, 'Ada') {
-    console.log(`Welcome, ${name} you are number (arr.length + 1) in line.`)
-}
-
-
-// ///////STEP #2 Create a function for line pass in the array of names katzDeli ///////////////////////
-
-function line(array) {
-    let i;
-    if (i = 0) {
+let array = ['Ada','Kent', 'Grace']
+function customerLine(array) {
+    if (array.length === 0) {
         console.log("The line is currently empty.")
+    } else{
+        console.log(`The line is currently: 1. ${array[0]} 2. ${array[1]} 3. ${array[2]}`)
     }
-    else if (i < array.lentgh) {
-        {
-            console.log("The line is currently: 1." + array[0] + "2." + array[1] + "3. " + array[3])
-        }
-    }
-
 }
 
-
-function line(katzDeli, "Kent") {
-    console.log("The line is currently: 1. Grace 2. Kent");
-}
+customerLine(katzDeli);
+customerLine(array);
 
 
+// Build takeANumber function 
+// 2 arguments (a)-array of currently line of people
+// (b)- string of the name of the person joining the end of the line. (Think add to the end of an array)
+// (Think Array Methods  array.push() or spread operator [...'existing array', "new element to be added"])
+
+// function takeANumber(array, name) {
+//     let position = array.push(name)
+//     console.log(array.push(name))
+//     console.log(`Welcome, ${name}. You are number ${position} in line.`)
+// }
+// NOTE the return value of using method array.push() is the length of the new array
+
+// takeANumber(array, 'jamond');
 
 
 
-/////////////////////////////////////STEP #3 Function Take A number ///////////////////////   // 
-function takeANumber(katzDeli, name)
 
 
 
-line Function(Add in string of Deli member names.)
-
-Input: an array
-Output: returns a string(the string should contain the name of person(1) and their place in line(2))
-if no names are passed into the array default to "The Line is currently empty"
-
-
-takeANumber Function
-Input: (1) array--(katzDeli)
-    (2) string - "Steve Young", "Troy Aikman", "Jada Pinkett",
-        *** The new person needs to be added to the end of the line.Use Array Method.push()
-    *** or Spread operator(...) Method ***
-
-
-}
-console.log(`Welcome, ${name}. You are number katzDeli[0]+1 in line.`)
-
-Output: (1) persons name and their positon in line.
-        *** Their position will be the index position in the array + 1
-
-Example in my string["Jerry Rice," "Emmitt Smith", "Will Smith"] 
-    *** Jerry Rice      Index Position is[0] position in line is 1. ***
-    *** Emmitt Smith    Index Position is[1] position in line is 2. ***
-    *** Will Smith      Index Position is[2] position in line is 3. ***
-
-    When I add in the new people it will be
-        *** Steve Young     Index Position is[3] position in line is 4. ***
-    *** Troy Aikman     Index Position is[4] position in line is 5. ***
-    *** Jada Pinkett    Index Position is[5] position in line is 6. ***
-
-    Output: "Welcome, ("name"). You are number 1 in line."
-
-// ///////////////////////////////////////////////////////////////////////////////
-
-nowServing(katzDeli){
-    console.log("Currently serving Grace")
-}
 
 
 
